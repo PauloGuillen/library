44c64185623490f0d1db3d457c9830a6808418b2
PauloGuillen <psm.guillen@gmail.com>
2023-08-22 09:31:38 -0300

    workflow run tests 1.18

--- libraryapi/core/views.py
+++ libraryapi/core/views.py
@@ -26,8 +26,6 @@ class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
     filter_backends = [BookFilterBackend]
-
-
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
